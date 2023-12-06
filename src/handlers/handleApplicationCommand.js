2d1f41c4a9fff36e7f1099af2503078477eb26d1
chrisae9 <chrisae9@gmail.com>
2023-12-06 06:16:30 +0000

    request logging

--- src/handlers/handleApplicationCommand.js
+++ src/handlers/handleApplicationCommand.js
@@ -4,9 +4,16 @@ const { InteractionResponseType } = require('discord-interactions');
 function handleApplicationCommand(interaction) {
     switch (interaction.data.name) {
         case 'service':
+            const userId = interaction.member.user.id;
+            const userName = interaction.member.user.username;
+            const commandName = interaction.data.name;
             const selectedService = interaction.data.options.find(opt => opt.name === 'name').value;
             const action = interaction.data.options.find(opt => opt.name === 'action').value;
-       
+            const dateTime = new Date().toISOString();
+        
+            // Log the command request details
+            console.log(`[${dateTime}] User ${userName} (${userId}) requested to run command '${commandName}' with service '${selectedService}' and action '${action}'`);
+        
             runDockerCompose(selectedService, action, interaction.token);
     
             // Immediately acknowledge the interaction
--- src/handlers/handleCommands.js
+++ src/handlers/handleCommands.js
@@ -8,18 +8,27 @@ require('dotenv').config();
 const ALLOWED_GUILD_IDS = process.env.ALLOWED_GUILD_IDS ? process.env.ALLOWED_GUILD_IDS.split(',') : [];
 const ALLOWED_ROLE_IDS = process.env.ALLOWED_ROLE_IDS ? process.env.ALLOWED_ROLE_IDS.split(',') : [];
 
-console.log('Guild IDs:', process.env.ALLOWED_GUILD_IDS);
-console.log('Role IDs:', process.env.ALLOWED_ROLE_IDS);
-
 function handleCommands(interaction) {
-    console.log('Handling Command:', interaction.data.name);
+    const userId = interaction.member.user.id;
+    const userName = interaction.member.user.username;
+    const dateTime = new Date().toISOString();
 
     if (interaction.type === InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE) {
         return handleAutocomplete(interaction);
     }
 
+    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+        const commandName = interaction.data.name;
+        const options = interaction.data.options || [];
+        const serviceOption = options.find(opt => opt.name === 'name')?.value || 'N/A';
+        const actionOption = options.find(opt => opt.name === 'action')?.value || 'N/A';
+
+        console.log(`[${dateTime}] User ${userName} (${userId}) requested to run command '${commandName}' with service '${serviceOption}' and action '${actionOption}'`);
+    }
+
     // Check if the interaction is from an allowed guild
     if (!ALLOWED_GUILD_IDS.includes(interaction.guild_id)) {
+        console.log(`[${dateTime}] Access denied for user ${userName} (${userId}) in guild ${interaction.guild_id}. Guild not allowed.`);
         const embed = createEmbed({
             title: "Access Restricted",
             description: "This command can't be used in this guild.",
@@ -35,8 +44,10 @@ function handleCommands(interaction) {
     }
 
     // Check if the member has any of the allowed roles
-    const memberHasAllowedRole = interaction.member.roles.some(role => ALLOWED_ROLE_IDS.includes(role));
+    const memberRoles = interaction.member.roles;
+    const memberHasAllowedRole = memberRoles.some(roleId => ALLOWED_ROLE_IDS.includes(roleId));
     if (!memberHasAllowedRole) {
+        console.log(`[${dateTime}] Access denied for user ${userName} (${userId}) in guild ${interaction.guild_id}. Role not allowed.`);
         const embed = createEmbed({
             title: "Permission Denied",
             description: "You don't have permission to use this command.",
@@ -55,7 +66,7 @@ function handleCommands(interaction) {
         return handleApplicationCommand(interaction);
     }
 
-    console.log('Unknown interaction type:', interaction.type);
+    console.log(`[${dateTime}] Unknown interaction type: ${interaction.type} for user ${userName} (${userId})`);
     return {
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
